43c35ad28bbc46b8b8101f1fc4860766158caf16
wsxingjun <1185889308@qq.com>
2018-11-23 18:53:55 +0800

    _007_QEventFilter:事件过滤器和事件的发送

---
+++ _007_QEventFilter/build-myeventfilter-Desktop_Qt_5_7_1_MinGW_32bit-Debug/ui_widget.h
@@ -0,0 +1,59 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.7.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QTextEdit *textEdit;
+    QSpinBox *spinBox;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QStringLiteral("Widget"));
+        Widget->resize(400, 300);
+        textEdit = new QTextEdit(Widget);
+        textEdit->setObjectName(QStringLiteral("textEdit"));
+        textEdit->setGeometry(QRect(70, 60, 104, 71));
+        spinBox = new QSpinBox(Widget);
+        spinBox->setObjectName(QStringLiteral("spinBox"));
+        spinBox->setGeometry(QRect(70, 150, 42, 22));
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QApplication::translate("Widget", "Widget", Q_NULLPTR));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
---
+++ _007_QEventFilter/myeventfilter/widget.cpp
@@ -0,0 +1,54 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+#include <QKeyEvent>
+#include <QWheelEvent>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    ui->textEdit->installEventFilter(this);        //为编辑器textEdit部件在本窗口安装事件过滤器
+    ui->spinBox->installEventFilter(this);         //为spinBox在本窗口安装事件过滤器
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+/**
+ * 事件过滤器：可以很容易的处理多个部件的多个事件
+ */
+bool Widget::eventFilter(QObject *watched, QEvent *event)
+{
+    if (watched == ui->textEdit){
+        if (event->type() == QEvent::Wheel){                                //判断部件
+            //将event事件强制转换为发生的事件的类型
+            QWheelEvent *wheelEvent = static_cast<QWheelEvent*>(event);
+            if (wheelEvent->delta() > 0){
+                ui->textEdit->zoomIn();                                     //滚轮滚动，则textEdit字体放大
+                return true;
+            }else {
+                ui->textEdit->zoomOut();
+                return false;
+            }
+        }
+    }else if (watched == ui->spinBox) {
+        if (event->type() == QEvent::KeyPress) {
+            QKeyEvent *keyEvent = static_cast<QKeyEvent*> (event);
+            if (keyEvent->key() == Qt::Key_Space){
+                ui->spinBox->setValue(0);                               //按下空格键，则spinBox的值设置为0
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }else {
+        return QWidget::eventFilter(watched,event);
+    }
+}
